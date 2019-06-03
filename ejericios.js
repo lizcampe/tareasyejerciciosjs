ec5afe97445e700233b7008c1332c35bcf71b791
lizcampe <lizcampe@gmail.com>
2019-06-03 01:26:55 -0500

    buu

--- ejericios.js
+++ ejericios.js
@@ -112,7 +112,7 @@ var numus = Number(prompt ("Ingresa un numero a sumar"));
 var n2=0;
              while (numus >= 0) {
               n2 += numus;
-              var numus= Number(prompt ("ingresa número"))
+        var numus= Number(prompt ("ingresa número"))
                   }
                  console.log(" La suma es: "+' ' + n2);
 
@@ -140,66 +140,121 @@ function ucFra( str )
 };
 console.log(ucFra(str));
 
- 
 
 // Revisa si un número dado es múltiplo de 3 o de 7
 
-function multiplo () {
-   let num = window.prompt ()
-}
+
+function multiple (valor, multiple) {
+  var valor = parseInt (prompt("Ingresa el número que quieres revisar"));
+  var multiple = parseInt (prompt("Introduce el mùltiplo que quieres explorar"));
+  resto= valor % multiple;
+    if(resto %3 ===0) {
+      alert ("el número " + valor + "es múltiplo de " + multiple);
+    } else if (resto %7 ===0){
+      alert ("el nùmero "+ valor + " es mùltiplo de " + multiple);
+      } else {
+        alert ("el nùmero que introdujiste no es mùltiplo de 3 o 7");
+      }
+    }
 
 
 
 
 
-
-
-
+ 
 // Revisa cuantas veces se repite un caracter dado en una cadena dada
+ var frase1= prompt ('inserta una frase');
+ var contando=0;
+ var x= prompt('Letra que quieres saber cuánto se repite');
+var  indice = frase1.indexOf(x);
+while (indice !=-1){
+  contando++;
+  indice= frase1.indexOf(x, indice+1);
+}
+document.writeln(`la letra "${x}" se repite ${contando} veces`);
+
 //var cadena =String(prompt('ingresa cadena de oración'));
+
 //var a= String(prompt('ingresa letra a buscar'));
 //cadena.split("a").length-1
 
-*/
+
 // Revisa cuantas veces se repite un valor dado en un arreglo
 
-/*
 
-var num =Number(prompt('ingresa num grande'));
-var w = Number(prompt('ingresa num a buscar'));
+
+
+
+
+
+var num =(prompt('ingresa num grande'));
+var w = (prompt('ingresa num a buscar'));
 console.log (num.split(w).length-1);
+
 
 // Divide un número de 3 dígitos en sus centenas, decenas y unidades
 
-function multiplo() {
-  let num= prompt('Ingresa número:')
-  num= 
-  // 103 -> 1 centena, 0 decenas, 3 unidades
+
+
+function numCentenas(numero) {
+  var num= numero.toString().split('');
+  var result= num[0]+ ' centenas' +', '+ num[1] + 'decenas' + ', '+ num [2] + ' unidades';
+  console.log(result);   
 }
+
+numCentenas (599)
 
 
 // Regresa todos los caracteres que no sean letras de una cadena
 
 
+let palabraNoletra = (prompt("ingresa caracteres"));
+var arregloNoletra = [];
+var arr = palabraNoletra.split('');
+
+for ( i= 0; i < arr.lenght; i++) {
+  if (/[a-zA-z]+/.test(arr[i])) {
+    arregloNoletra= arregloNoletra
+  } else {
+    arregloNoletra += arr[i]
+  }
+}
+document.getElementByID('car').innerHTML = arregloNoletra;
+
+
+
+
+
+
 // Haz una función que lanza un error con el mensaje dado por el usuario
-function 
+
+let mensaje = window.prompt("ingrese su mensaje de error: ");
+function errorImp(){
+  throw new Error(mensaje);
+};
+
+
+
 
 // Extiende la función anterior para atrapar el error e imprimir su mensaje y stack
-*/
-/*
 
-try{ }
-
-catch (error){ alert;  }
-
+function errorImp(){
+try{ 
+error()
+}catch (error){ 
+  document.getElementById('err').innerHTML = Error;
+  document.getElementById('errStack').innerHTML =Error.stack;
+  }
+}
 
 
 // Suma los contenidos de un arreglo de números
-var losNumeros = Arr();
+
+var losNumeros = Array();
 var suma = 0;
 function sumandoNumeros() {
   for (n=0; n<5; n++){
-  losNumeros[5]=Number(prompt('escribe un número'))
+  losNumeros[5]=Number(prompt('escribe un número'));
 }
  for (i= 0; i < losNumeros.length; i++) {
   suma+= losNumeros[i];
@@ -207,17 +262,46 @@ function sumandoNumeros() {
  return suma
 }
  alert ('la suma es ' + sumandoNumeros());
+
+
+
 // Regresa un arreglo nuevo con el cuadrado de cada valor del arreglo original
 
-l
+function cuadrado() {
+  let arr =window.prompt ('Ingresa el arreglo a modificar: ');
+  var cuad = [];
+  for (let i=0; i<arr.length; i++) {
+  if (/\d/.test(arr[i])) {
+  cuad.push(Math.pow(parseFloat(arr[i]), 2));
+  }
+  }
+  document.getElementById('resultado').innerHTML =cuad;
+}
+
+ 
 
 // Regresa que tipo de ángulo es el dado
-/*
+
   Agudo: 0 a 90 grados
   Recto: 90 grados
   Obtuso: 90 a 180 grados
   Llano: 180 grados
 
+
+var angulos = Number(prompt ("ingresa el número de grados del ángulo"));
+
+if (angulos>=0 && angulos <= 89){
+  alert ('Angulo agudo')
+} else if (angulos ==90){
+  alert ('Angulo recto') 
+} else if (angulos <= 179 && angulos > 90) {
+  alert ('Aangulo obtuso')
+} else if (angulos ==180) {
+  alert ('Angulo llano')
+}
+  else {
+  alert ('Ingrese un àngulo entre 0 y 180')
+  };
 
 
 
@@ -225,11 +309,36 @@ l
 
 // Regresa un arreglo nuevo sin los valores repetidos de un arreglo original
 
-//arr 
+function sinRepetidos() {
+  var elementos = null;
+  var arreglo = [];
+   while (elementos >= 0) {
+    if (elementos != null) {
+      arreglo.push(elementos);
+      } 
+      elementos = prompt ('Dame un número: ');
+      }
+    elSet = new Set(arreglo);
+    var resultadosSinRepetir = JSON.stringify(Array.from(elSet));
+    document.getElementById("resultadosSinRepetir").innerHTML = resultadosSinRepetir;
+
+}
 
 
 // Quita los valores repetidos de un arreglo (sin usar otro arreglo)
 //(el del set)
+
+function sinRepetido2(arreglo):
+for (var i in arreglo) (
+  for (var j in arreglo) {
+  if (arreglo [i]==arreglo[j]&& 1 !=j) [
+   j++;
+   arreglo.splice(i.1);
+  ]
+  }
+}
+return arreglo;
+}
 
 // Convierte un número binario dado por el usuario a decimal
 
@@ -250,7 +359,7 @@ alert( num.toString(16) );
 
 
 // Regresa la cantidad de valores que comparten dos arreglos diferentes
-*/
+
 
 var array =[5,5,4,7,8];
 var thisArg= [5,3,5,5,2,5,3];
@@ -265,20 +374,9 @@ var thisArg= [5,3,5,5,2,5,3];
 console.log(resultado);
  
 
-/*
 
 
-array1 = ["uno", "dos", "tres"];    
-array2 = ["tres", "dos", "uno"];
 
-array1.sort();
-array2.sort();
-
-
-array1.length==array2.length && array1.every(function(v,i)
-  { return v === array2[i] }
-
-*/
 
 // Valida que una cadena dada no tenga espacios en blanco
 
@@ -291,17 +389,64 @@ var a =String
 
 
 
-/*
+
 // Dada una cadena, determina su valor de scrabble
 
-  1: E, A, I, O, N, R, T, L, S, U
-  2: D, G
-  3: B, C, M, P
-  4: F, H, V, W, Y
-  5: K
-  8: J, X
-  10: Q, Z
-*/
+  */
+//npm install -g jasmine
+//jasmine scrabble-score.spec.js
+
+
+
+  const valorporletra = {
+  1: [E, A, I, O, N, R, T, L, S, U],
+  2: [D, G],
+  3: [B, C, M, P],
+  4: [F, H, V, W, Y],
+  5: [K],
+  8: [J, X],
+  10: [Q, Z]
+  };
+/*VAR: count scores*/
+var sum;
+
+/*FUNCTION: return score of letter*/
+function lettersToScore(letter) 
+{
+    for (var index in letters)
+    {
+  if (Object.keys(letters).indexOf((letters[index].indexOf(letter) !== -1) ? index.toString() : '-1')
+      !== -1) {
+      return parseInt(index);
+  } 
+    }
+}
+
+/*FUNCTION: count final score*/
+var score = function(input) {
+    sum = 0;
+    if (input === null || input === "") {
+  return 0;
+    }
+    
+    input.toUpperCase().split("").forEach(function(elem, index, array) {
+  sum += lettersToScore(elem);
+    });
+    return sum;
+}
+
+/*EXPORTS: export of function score*/
+module.exports = score;
+
+
+const calcScore = (text) => {
+    let score = 0;
+    text = text.toLowerCase();
+    for (let ch of text) {
+        score += valorporletra[ch];
+    }
+    return score;
+}
 
 
 // Determina si una cadena dada por el usuario es un acrónimo
@@ -312,6 +457,4 @@ var a =String
 
 // Implementa una lista doblemente ligada
 
-
 // Obten el Máximo común divisor de dos números dados
-
